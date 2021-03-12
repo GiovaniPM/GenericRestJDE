afc8ef16c240fbb9ee3dfee4f148c2f59004196b
Giovani Perotto Mesquita <giovanipm@gmail.com>
2021-03-12 14:27:27 -0300

    giova(DESKTOP-PG18CKU) 210312142727

--- api/api.py
+++ api/api.py
@@ -168,7 +168,7 @@ def home():
     Heards - <mark>{"Content-Type":"application/json"}</mark><br>
     Method - <mark>GET</mark><br>
     Body - <mark>{ "object": "F4111", "filter": [ { "operator": ">", "term1": "TAB.ILCRDJ", "term2": 118000 }, { "operator": "AND", "term1": null, "term2": null }, { "operator": "<", "term1": "TAB.ILCRDJ", "term2": 119000 } ], "order": null, "data": [ { "column": "TAB.ILITM", "value": null }, { "column": "TAB.ILLITM", "value": null }, { "column": "TAB.ILMCU", "value": null }, { "column": "TAB.ILCRDJ", "value": null } ] }</mark></div>
-    <div style="font-family:Verdana;font-size:120%;"><br>- GET information <a href="http://127.0.0.1:5000/api/v2/oracle/select">http://127.0.0.1:5000/api/v1/oracle/select</a></div>
+    <div style="font-family:Verdana;font-size:120%;"><br>- GET information <a href="http://127.0.0.1:5000/api/v2/oracle/select">http://127.0.0.1:5000/api/v2/oracle/select</a></div>
     Ex.:<br>
     <div style="font-family:Verdana;background-color:white;color:black;padding:30px;">URL - <mark>http://127.0.0.1:5000/api/v2/oracle/select</mark><br>
     Heards - <mark>{"Content-Type":"application/json"}</mark><br>
