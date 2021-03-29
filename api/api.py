97f72b155338cba6f33fdc262190be83af351a94
Giovani <giovanipm@gmail.com>
2021-03-29 18:20:27 -0300

    giova(DESKTOP-PG18CKU) 210329182027

--- api/api.py
+++ api/api.py
@@ -12,12 +12,14 @@ import sys
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = False
-#logging.basicConfig(level=logging.DEBUG)
+# logging.basicConfig(level=logging.DEBUG)
+
 
 class Binders:
     activate = True
     parameters = {}
     next = 0
+
 
 class DBKeys:
     db_host = 'localhost'
@@ -26,47 +28,58 @@ class DBKeys:
     db_user = 'C##GIOVANIPM'
     db_pass = 'Pm11092j'
 
+
 class Errors:
     qty = 0
     list = []
+
 
 class Error(Exception):
     """Base class for other exceptions"""
     pass
 
+
 class NoColumnedQuery(Error):
     """Return column(s) are expected!"""
     pass
+
 
 class NoFilteredQuery(Error):
     """Filter column(s) are expected!"""
     pass
 
+
 class NoInsertedValuesQuery(Error):
     """Insert value(s) and/or column(s) are expected!"""
     pass
+
 
 class NoUpdatedValuesQuery(Error):
     """Update value(s) and/or column(s) are expected!"""
     pass
 
+
 class DateBadFormated(Error):
     """Date Must be YYYY-MM-DD!"""
     pass
+
 
 class TimeBadFormated(Error):
     """Date Must be HH:mm:SS!"""
     pass
 
+
 class GenericError(Error):
     """This is a generic error!"""
     pass
 
+
 def thereisJson():
     if request.environ['CONTENT_LENGTH'] == '0':
-        errorCatch(500.07,"JSON in BODY is required!")
+        errorCatch(500.07, "JSON in BODY is required!")
         raise GenericError
     return ""
+
 
 def findWord(dictseq, txt):
     for word in dictseq:
@@ -74,11 +87,13 @@ def findWord(dictseq, txt):
             return True
     return False
 
-def findValue(dictseq,txt):
+
+def findValue(dictseq, txt):
     for word in dictseq:
         if dictseq[word] == txt:
             return True
     return False
+
 
 def errorCatch(number, text):
     reg = {}
@@ -88,12 +103,14 @@ def errorCatch(number, text):
     Errors.qty = Errors.qty + 1
     return True
 
+
 def clearDefinitions():
     global Binders, Errors
     Binders.parameters = {}
     Binders.next = 0
     Errors.list = []
     Errors.qty = 0
+
 
 def createConnection():
     try:
@@ -113,33 +130,39 @@ def createConnection():
                     )" % (DBKeys.db_host, str(DBKeys.db_port), DBKeys.db_servicename)
     return cx_Oracle.connect(user=DBKeys.db_user, password=DBKeys.db_pass, dsn=conn_string, encoding='UTF-8')
 
+
 def outputLog(text):
     text = str(text)
     print(" ".join(text.split()))
 
+
 def removeExtendCharacters(txt):
     return normalize('NFKD', txt).encode('ASCII', 'ignore').decode('ASCII')
 
-def datestdtojd (stddate):
-    fmt='%Y-%m-%d'
+
+def datestdtojd(stddate):
+    fmt = '%Y-%m-%d'
     sdtdate = datetime.datetime.strptime(stddate, fmt)
-    nmyear=sdtdate.year
+    nmyear = sdtdate.year
     sdtdate = sdtdate.timetuple()
     jdate = sdtdate.tm_yday
     return(nmyear*1000+jdate)
 
-def jdtodatestd (jdate):
+
+def jdtodatestd(jdate):
     fmt = '%Y%j'
     datestd = datetime.datetime.strptime(jdate, fmt).date()
     return(datestd)
 
-def timestdtonum (stddate):
-    fmt='%H:%M:%S'
+
+def timestdtonum(stddate):
+    fmt = '%H:%M:%S'
     stddate = datetime.datetime.strptime(stddate, fmt)
-    mmHour=stddate.hour
-    mnMinute=stddate.minute
-    mnSecond=stddate.second
+    mmHour = stddate.hour
+    mnMinute = stddate.minute
+    mnSecond = stddate.second
     return(mnSecond+100*mnMinute+10000*mmHour)
+
 
 def valid_date(datestring):
     try:
@@ -148,12 +171,14 @@ def valid_date(datestring):
     except ValueError:
         return False
 
+
 def valid_time(datestring):
     try:
         datetime.datetime.strptime(datestring, '%H:%M:%S')
         return True
     except ValueError:
         return False
+
 
 def binderCreate(txt):
     global Binders
@@ -163,43 +188,45 @@ def binderCreate(txt):
     Binders.parameters[key] = txt
     return ":" + bindersName
 
+
 def defineType(txt):
     if txt != None:
         if isinstance(txt, (int, float)):
             return "int"
         elif txt.find("TAB.") == 0:
             return "tab"
-        elif txt.find("DTA.") == 0: 
+        elif txt.find("DTA.") == 0:
             return "dta"
-        elif txt.find("TME.") == 0: 
+        elif txt.find("TME.") == 0:
             return "tme"
-        elif txt.find("CNJ.") == 0: 
+        elif txt.find("CNJ.") == 0:
             return "cnj"
         else:
             return "str"
+
 
 def removePrefix(term):
     if Binders.activate == True:
         if defineType(term) == "int":
             term = binderCreate(str(term))
         elif defineType(term) == "tab":
-            term = term.replace("TAB.","")
+            term = term.replace("TAB.", "")
         elif defineType(term) == "dta":
-            term = term.replace("DTA.","")
+            term = term.replace("DTA.", "")
             if valid_date(term):
                 term = binderCreate(str(datestdtojd(term)-1900000))
             else:
                 term = binderCreate(str(999999))
                 errorCatch(500.05, "Date must be YYYY-MM-DD format!")
         elif defineType(term) == "tme":
-            term = term.replace("TME.","")
+            term = term.replace("TME.", "")
             if valid_time(term):
                 term = binderCreate(str(timestdtonum(term)))
             else:
                 term = binderCreate(str(999999))
                 errorCatch(500.06, "Time must be HH:mm:SS format!")
         elif defineType(term) == "cnj":
-            term = term.replace("CNJ.","")
+            term = term.replace("CNJ.", "")
         else:
             term = binderCreate(term.strip())
         return term
@@ -207,26 +234,27 @@ def removePrefix(term):
         if defineType(term) == "int":
             term = str(term)
         elif defineType(term) == "tab":
-            term = term.replace("TAB.","")
+            term = term.replace("TAB.", "")
         elif defineType(term) == "dta":
-            term = term.replace("DTA.","")
+            term = term.replace("DTA.", "")
             if valid_date(term):
                 term = str(datestdtojd(term)-1900000)
             else:
                 term = str(999999)
                 errorCatch(500.05, "Date must be YYYY-MM-DD format!")
         elif defineType(term) == "tme":
-            term = term.replace("TME.","")
+            term = term.replace("TME.", "")
             if valid_time(term):
                 term = str(timestdtonum(term))
             else:
                 term = str(999999)
                 errorCatch(500.06, "Time must be HH:mm:SS format!")
         elif defineType(term) == "cnj":
-            term = term.replace("CNJ.","")
+            term = term.replace("CNJ.", "")
         else:
             term = "'" + term + "'"
         return term
+
 
 def makeWhere(operator, term1, term2):
     if Binders.activate == True:
@@ -281,6 +309,7 @@ def makeWhere(operator, term1, term2):
     elif operator == "NOT IN":
         return term1 + " NOT IN " + term2
 
+
 def makeSet(term1, term2):
     if term1 != None:
         term1 = removePrefix(term1)
@@ -288,10 +317,11 @@ def makeSet(term1, term2):
         term2 = removePrefix(term2)
     return term1 + " = " + term2
 
+
 def makeOrder(column, order):
     object_value = column
     if object_value.find("TAB.") == 0:
-        object_value = object_value.replace("TAB.","")
+        object_value = object_value.replace("TAB.", "")
     if order == "A":
         object_value = object_value + " ASC,"
     elif order == "D":
@@ -299,6 +329,7 @@ def makeOrder(column, order):
     else:
         object_value = object_value + ","
     return object_value
+
 
 def makeHeader(json_data):
     output_list = json_data["data"]
@@ -308,18 +339,20 @@ def makeHeader(json_data):
         if isinstance(header, (int, float)):
             header = str(header)
         elif header.find("TAB.") == 0:
-            header = header.replace("TAB.","")
+            header = header.replace("TAB.", "")
         else:
             header = "'" + header + "'"
         objects_list.append(header)
     return objects_list
 
+
 def qtyHeader(json_data):
     output_list = json_data["data"]
     qty = 0
     for json_object in output_list:
-        qty =  qty + 1
+        qty = qty + 1
     return qty
+
 
 def makeSelect(json_data):
     object_name = json_data["object"]
@@ -333,7 +366,7 @@ def makeSelect(json_data):
         for json_object in output_list:
             object_value = json_object["column"]
             if object_value.find("TAB.") == 0:
-                object_value = object_value.replace("TAB.","")
+                object_value = object_value.replace("TAB.", "")
             sql_string = sql_string + " " + object_value + ","
         sql_string = sql_string + " rownum "
         sql_string = sql_string + " FROM "
@@ -341,18 +374,21 @@ def makeSelect(json_data):
         if filter_list != None:
             sql_string = sql_string + " WHERE "
             for json_object in filter_list:
-                object_value = makeWhere(json_object["operator"], json_object["term1"], json_object["term2"])
+                object_value = makeWhere(
+                    json_object["operator"], json_object["term1"], json_object["term2"])
                 sql_string = sql_string + " " + object_value
         if order_list != None:
             sql_string = sql_string + " ORDER BY "
             for json_object in order_list:
-                object_value = makeOrder(json_object["column"], json_object["sort"])
+                object_value = makeOrder(
+                    json_object["column"], json_object["sort"])
                 sql_string = sql_string + " " + object_value
             sql_string = sql_string + " 1 "
     if Errors.qty > 0:
         raise GenericError
     else:
         return sql_string
+
 
 def makeUpdate(json_data):
     object_name = json_data["object"]
@@ -367,21 +403,25 @@ def makeUpdate(json_data):
     else:
         for json_object in output_list:
             if update_list == "":
-                update_list = makeSet(json_object["column"], json_object["value"])
+                update_list = makeSet(
+                    json_object["column"], json_object["value"])
             else:
-                update_list = update_list + ", " + makeSet(json_object["column"], json_object["value"])
+                update_list = update_list + ", " + \
+                    makeSet(json_object["column"], json_object["value"])
     sql_string = sql_string + " " + update_list
     if filter_list == None:
         errorCatch(500.02, "Filter column(s) are required!")
     else:
         sql_string = sql_string + " WHERE "
         for json_object in filter_list:
-            object_value = makeWhere(json_object["operator"], json_object["term1"], json_object["term2"])
+            object_value = makeWhere(
+                json_object["operator"], json_object["term1"], json_object["term2"])
             sql_string = sql_string + " " + object_value
     if Errors.qty > 0:
         raise GenericError
     else:
         return sql_string
+
 
 def makeInsert(json_data):
     object_name = json_data["object"]
@@ -393,11 +433,13 @@ def makeInsert(json_data):
     if output_list == None:
         errorCatch(500.03, "Insert column(s) are required!")
     else:
+
         for json_object in output_list:
             if insert_list == "":
                 insert_list = removePrefix(json_object["column"])
             else:
-                insert_list = insert_list + ", " + removePrefix(json_object["column"])
+                insert_list = insert_list + ", " + \
+                    removePrefix(json_object["column"])
         sql_string = sql_string + " " + insert_list
         sql_string = sql_string + " ) VALUES ( "
         insert_list = ""
@@ -405,7 +447,8 @@ def makeInsert(json_data):
             if insert_list == "":
                 insert_list = removePrefix(json_object["value"])
             else:
-                insert_list = insert_list + ", " + removePrefix(json_object["value"])
+                insert_list = insert_list + ", " + \
+                    removePrefix(json_object["value"])
         sql_string = sql_string + " " + insert_list
         sql_string = sql_string + " ) "
     if Errors.qty > 0:
@@ -413,23 +456,25 @@ def makeInsert(json_data):
     else:
         return sql_string
 
+
 def makeDelete(json_data):
     object_name = json_data["object"]
     filter_list = json_data["filter"]
-    output_list = json_data["data"]
     sql_string = "DELETE FROM "
     sql_string = sql_string + object_name
+    sql_string = sql_string + " WHERE "
     if filter_list == None:
         errorCatch(500.02, "Filter column(s) are required!")
     else:
-        sql_string = sql_string + " WHERE "
         for json_object in filter_list:
-            object_value = makeWhere(json_object["operator"], json_object["term1"], json_object["term2"])
+            object_value = makeWhere(
+                json_object["operator"], json_object["term1"], json_object["term2"])
             sql_string = sql_string + " " + object_value
     if Errors.qty > 0:
         raise GenericError
     else:
         return sql_string
+
 
 @app.route('/api/v1/oracle/select', methods=['GET'])
 def api_oracle_select():
@@ -444,7 +489,7 @@ def api_oracle_select():
     except Exception as e:
         cur.close()
         conn.close()
-        errorCatch(-1,format(e))
+        errorCatch(-1, format(e))
         return jsonify(Errors.list), 406
     if app.config["DEBUG"] == True:
         outputLog(flask.request.json)
@@ -457,7 +502,7 @@ def api_oracle_select():
             cur.execute(None, Binders.parameters)
         else:
             cur.execute(None, {})
-        rv = cur.fetchall()    
+        rv = cur.fetchall()
     except cx_Oracle.DatabaseError as e:
         errorObj, = e.args
         errorCatch(errorObj.code, errorObj.message)
@@ -480,6 +525,7 @@ def api_oracle_select():
         conn.close()
     return jsonify(objects_list), 200
 
+
 @app.route('/api/v1/oracle/update', methods=['PUT'])
 def api_oracle_update():
     clearDefinitions()
@@ -490,10 +536,10 @@ def api_oracle_update():
         sql_string = makeUpdate(flask.request.json)
     except GenericError:
         return jsonify(Errors.list), 406
-    except Exception as e:    
+    except Exception as e:
         cur.close()
         conn.close()
-        errorCatch(-1,format(e))
+        errorCatch(-1, format(e))
         return jsonify(Errors.list), 406
     if app.config["DEBUG"] == True:
         outputLog(flask.request.json)
@@ -516,6 +562,7 @@ def api_oracle_update():
     cur.close()
     conn.close()
     return jsonify([]), 200
+
 
 @app.route('/api/v1/oracle/insert', methods=['POST'])
 def api_oracle_insert():
@@ -527,10 +574,10 @@ def api_oracle_insert():
         sql_string = makeInsert(flask.request.json)
     except GenericError:
         return jsonify(Errors.list), 406
-    except Exception as e:    
+    except Exception as e:
         cur.close()
         conn.close()
-        errorCatch(-1,format(e))
+        errorCatch(-1, format(e))
         return jsonify(Errors.list), 406
     if app.config["DEBUG"] == True:
         outputLog(flask.request.json)
@@ -553,6 +600,7 @@ def api_oracle_insert():
     cur.close()
     conn.close()
     return jsonify([]), 200
+
 
 @app.route('/api/v1/oracle/delete', methods=['DELETE'])
 def api_oracle_delete():
@@ -564,10 +612,10 @@ def api_oracle_delete():
         sql_string = makeDelete(flask.request.json)
     except GenericError:
         return jsonify(Errors.list), 406
-    except Exception as e:    
+    except Exception as e:
         cur.close()
         conn.close()
-        errorCatch(-1,format(e))
+        errorCatch(-1, format(e))
         return jsonify(Errors.list), 406
     if app.config["DEBUG"] == True:
         outputLog(flask.request.json)
@@ -591,11 +639,12 @@ def api_oracle_delete():
     conn.close()
     return jsonify([]), 200
 
+
 @app.route('/', methods=['GET'])
 def home():
     #app.logger.warning('A warning occurred (%d apples)', 42)
     #app.logger.error('An error occurred')
-    #app.logger.info('Info')
+    # app.logger.info('Info')
     return '''
     <h1>Web Service Directory</h1>
     To see more information please visite the <a href="https://github.com/GiovaniPM/GenericRestJDE">project site.</a><br>
@@ -651,6 +700,7 @@ def home():
         </div>
     </div>
     '''
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=os.environ.get('PORT', '8080'))
